077c84a34cc6de1357a3c82447acfae2a61fa40b
Ben <benjaminchil91@gmail.com>
2024-01-14 19:26:50 +0100

    redesign timeline

--- script.js
+++ script.js
@@ -3,5 +3,18 @@ document.querySelectorAll('.open').forEach(function (openElement) {
     var container = this.closest('.topic');
     var answer = container.querySelector('.answer');
     var trigger = container.querySelector('.faq-t');
+
+    // Toggle the 'expanded' class on the topic container
+    container.classList.toggle('expanded');
+
+    // Toggle the display of the answer
+    if (answer.style.display === 'none' || answer.style.display === '') {
+      answer.style.display = 'block';
+    } else {
+      answer.style.display = 'none';
+    }
+
+    // Toggle the rotation of the trigger element
+    trigger.classList.toggle('faq-o');
   });
 });
